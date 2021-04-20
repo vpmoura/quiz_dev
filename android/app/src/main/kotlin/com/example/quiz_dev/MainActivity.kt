52e2044e033347079b790633b64d0318037ebc76
Vinicius Moura <55149561+vpmoura@users.noreply.github.com>
2021-04-20 00:50:22 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/quiz_dev/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.quiz_dev
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home/widgets/chart/chart_widget.dart
@@ -0,0 +1,38 @@
+import 'package:DevQuiz/core/app_colors.dart';
+import 'package:DevQuiz/core/app_text_styles.dart';
+import 'package:flutter/material.dart';
+
+class ChartWidget extends StatelessWidget {
+  const ChartWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 70,
+      width: 70,
+      child: Stack(
+        children: [
+          Center(
+            child: Container(
+              height: 70,
+              width: 70,
+              child: CircularProgressIndicator(
+                strokeWidth: 7,
+                value: .75,
+                backgroundColor: AppColors.chartSecondary,
+                valueColor:
+                    AlwaysStoppedAnimation<Color>(AppColors.chartPrimary),
+              ),
+            ),
+          ),
+          Center(
+            child: Text(
+              "75%",
+              style: AppTextStyles.heading,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
